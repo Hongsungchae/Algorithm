21c133e4d399cebb6ca19e09acf385a4890283d0
홍성채 <97865622+Hongsungchae@users.noreply.github.com>
2023-03-23 16:15:12 +0900

    [Bronze III] Title: 최댓값, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int a[] = new int[9];
+		
+		
+		for(int i = 0; i< 9; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		int max = a[0];
+		int num = 0;
+		for(int i = 0; i< 9; i++) {
+			if(max < a[i]) {
+				max = a[i];
+				num = i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(num+1);
+	}
+}

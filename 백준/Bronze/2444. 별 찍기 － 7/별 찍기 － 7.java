cd0e013f17074bdd9cd3b8c29897246aead26b7d
홍성채 <97865622+Hongsungchae@users.noreply.github.com>
2023-03-27 15:45:52 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 704 ms, Memory: 24792 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		
+		for(int i = 1; i <= a ; i++) {
+			for(int j = 0; j < a-i; j++)
+				System.out.print(" ");
+			for(int j = 0; j < i*2-1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+		
+		for(int i = a-1; i >= 0 ; i--) {
+			for(int j = 0; j < a-i; j++)
+				System.out.print(" ");
+			for(int j = 0; j < i*2-1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+		
+	}
+	
+}
